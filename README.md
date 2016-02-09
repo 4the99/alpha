bc295683c6f3826707d9ebade404ddcbf9a85ab4
~bugnuts~ <bugnuts@4the99.org>
2016-02-09 13:05:33 +1100

    index & readme updates

--- README.md
+++ README.md
@@ -2,30 +2,35 @@ Welcome to https://4the99.org
 
 Due to the massive learning curve to make code absolutely secure, I have decided to go open source. 
 
-Any help or feedback is welcome, if you wish to chip in send 4the99/admin a message we would be vastly grateful for it. 
+Any help or feedback is welcome, if you wish to chip in send 4the99/admin a message/email we would be vastly grateful for it. 
+
+
 
 All code must follow the following principles: 
 
-Security without compromise. If it can't be done secure, then we won't do it until we can.
+Security without compromise. 
+If it can't be done secure, then we won't do it until we can.
 We will do everything I can to make sure everything is absolutely secure, however we are just mere mortals, We will need the help. Most of the delays are because of depression/security concerns and the vast amount of research and upskilling ongoing into protecting every layer (not just the site) from every known attack we can think of theoretical or not. Ultimate secuity is after all the holy grail, a lofty goal and perhaps a delusional fantasty, but we can try, we can try. 
 
-Personally Identifiable Information (PID) of our users is sacrosanct.
+Personally Identifiable Information (PII) of our users is sacrosanct.
 If a user puts in any information that could be used to trace them back to their true identity, we protect it with anything and everything that we feasible can do so. (economies of scale) 
 
 Users have complete control of their information. 
-All users can see, edit, and modify virtually everything we collect about them. The few rare exceptions to this rule is on the level of some dumb jihadi scum. Situations like this are simple, if they are a threat to them selves, others, general public, or our infrastructure we may reluctantly forced to breach their privacy and to take action for the betterment of all. 
+All users can see, edit, and modify virtually everything we collect about them. The few rare exceptions to this rule is on the level of some dumb jihadi scum. Situations like this are simple, if they are a threat to them selves, others, general public, or our infrastructure we may reluctantly forced to breach their privacy and to take action. The net is the net, the world is the world. Problems will arise weather we want them to or not. 
 
 Minimalist disclosure to 3rd parties. 
-For example, It is not strictly necessary to disclose the date of birth, or name, or virtually any details to ship a package, other than where to send it, and valid payment has been received. We fully indeed to be a privacy shield against the internet to much information is collected stored in insecure databases, websites are getting hacked right and center "big data" tracking and collection is everywhere and for sale. The more we can protect and minimize to keep our uses safe the better. 
+For example, It is not strictly necessary to disclose the date of birth, or name, or virtually any details to ship a package, other than where to send it, and valid payment has been received. We fully indeed to be a privacy shield against the internet to much information is collected stored in insecure databases, websites are getting hacked right and center "big data" tracking and collection is everywhere and for sale. The more we can protect and minimize to keep our users safe the better. 
 
-Maximum usability and customization (if so desired) some people like things simple and easy, others like tweaking everything we wish to cater to both worlds, nice and simple yet can be modified to however they like (within reason) 
+Maximum usability and customization (if so desired)
+Some people like things simple and easy, others like tweaking everything we wish to cater to both worlds, nice and simple yet can be modified to however they like (within reason) 
 
 If in doubt, deny access. 
 For example one person may like to keep chat logs, the other doesn't, in communication between both of them, no chat logs will be kept. 
 
 Personal safety is everything. 
-There will be vast, amounts of work into keeping our users safe, and we have many creative solutions to implement, everything from walking home late at night, to helping search and rescue in the middle of a natural disaster, to critical life saving information when seconds count. 
+There will be vast, amounts of work into keeping our users safe, and we have many creative solutions to implement, everything from walking home late at night, to helping search and rescue in the middle of a natural disaster, to critical life saving information when seconds count. Such things especially around gps techonogy are opt in/only activated when lives are in imment danger.
 
-Open Book financial records, (live records) That said, user privacy is also a major contributing factor so some things will have to be generalized to state/country for tax purposes (ecept for the user themselves where they can track the money they have spent/saved/donated/earned etc.
+Open Book financial records, (live records) 
+That said, user privacy is also a major contributing factor so some things will have to be generalized to state/country for tax purposes (ecept for the user themselves) where they can track the money they have spent/saved/donated/earned etc.
 
 So, So, So............. Much more to say, but happy xmass! Watch this space it will! be interesting. 
--- index.php
+++ index.php
@@ -9,89 +9,79 @@
 <!-- but i have to know, because i cant find an answer and just are not sure -->
 
 <?php
-header('Content-type: text/html; charset=utf-8'); PHP_EOL;  
-header allways append date_default_timezone_set("UTC");
-header always append X-Frame-Options SAMEORIGIN;
-header always append X-Frame-Options DENY;
-header always append X-Forwarded-Proto: https;
-header always append X-Forwarded-Port: 443;
-header always append Content-Security-Policy 
-header always append X-WebKit-CSP
-header always append X-Content-Security-Policy
-header always append Strict-Transport-Security: max-age=1000; includeSubDomains;
-//syntax correct?
+    header('Content-type: text/html; charset=utf-8'); PHP_EOL;  
+    header allways append date_default_timezone_set("UTC");
+    header always append X-Frame-Options SAMEORIGIN;
+    header always append X-Frame-Options DENY;
+    header always append X-Forwarded-Proto: https;
+    header always append X-Forwarded-Port: 443;
+    header always append Content-Security-Policy 
+    header always append X-WebKit-CSP
+    header always append X-Content-Security-Policy
+    header always append Strict-Transport-Security: max-age=1000; includeSubDomains;
+    //syntax correct?
 ?>
 
 </head>
 <body>
 <?php
-// starts over from scratch
-?>
-<!-- no pointer to public dirty vars yet --> 
-<form method="POST" action="" id="Login">
-	<input type="email" name="dirty_email" placeholder="Email"/>
-	<input type="password" name="dirty_password" placeholder="******"/>
-	<button name="dirty_login">Login</button> 
 
-<!-- need to make sure thais is ok -->
-<!-- http://stackoverflow.com/questions/121203/how-to-detect-if-javascript-is-disabled -->
-	<form onsubmit="this.js_enabled.value=2;return true;">
-    <input type="hidden" name="js_enabled" value="1">
-    <input type="submit" value="go">
-</form>
-</form>
-<!-- remember to hash before sending over to server --> 
+// input hidden, unless js is enabled, if form is submitted, but dirty_dirty_js_is_enabled !2 then its a bot
+// http://stackoverflow.com/questions/121203/how-to-detect-if-javascript-is-disabled
+// https://stackoverflow.com/questions/22108118/displaying-a-form-only-if-javascript-is-enabled
+
+?>
+
+<script type="text/javascript" >
+    document.getElementById('non_js_is_hidden').style.display='block';
+</script>
+
+
+<div id="non_js_hidden" style="display:none">
+    <form method="POST" action="" id="Login">
+        <input type="email" name="dirty_email" placeholder="Email"/>
+        <input type="password" name="dirty_password" placeholder="******"/>
+        <button name="dirty_login">Login</button> 
+
+        <form onsubmit="this.dirty_js_enabled.value=2;return true;">
+            <input type="hidden" name="js_enabled" value="1">
+            <input type="submit" value="go">
+        </form>
+    </form>
+</div>
 <br />
 <br />
 <div id="container">
-		<div id="block">
-			<div id="txt">
-			<br />
-			This is earth. Over 7 billion people live here. <br />
-			A child starves to death every 4 seconds.<br />
-			21 children die every second of every day from diarrhoea. <br />
-			123 children die every second before they reach the age of 5.<br />
-			over a billion people cant read or even write their own name.<br />
-			with 1% of the of the money spent on weapons, every child could be in school<br />
-			1.1 billion people need a proper water supply.<br />
-			2.6 billion people need basic sanitation.<br />
-			At least 80% of humanity lives on less than $10 a day.<br />
-			Almost half of the world population live on less than $2.50 a day.<br />
-			More than 1.3 billion live in extreme poverty less than $1.25 a day.<br />
-			50% of the worlds population account for less than 1% of the worlds wealth.<br />
-			the top 1% of the worlds population accounts for just under half the worlds wealth.<br />
-			the top 10% of the worlds population holds almost 90% of the worlds wealth.<br />
-			Time to make a change.<br />
-			<br />
-			No more bottom line thinking, and shareholder profits, the worlds most desperate people are our dividend. <br />
-			We are not a standard charity begging for money, we believe in self sustaining charity.<br />
-			We believe in creating a self sustaining pool of income that’s always there and ever growing to make a change and make a difference. <br />
-			The seed that we will plant to grow the forest, here, now, today is simple. <br />
-			we will happily provide feature rich technological solutions to make your life better<br />
-			by simply using our services you change the world<br />
-			make a difference today, join now<br />
-			<br />
-			<br />
-			****under active development please check back soon****
-			<br />
-			<br />
-just dumping to keep <br />
-Across the world, billions of people are starving to death. <br />
-Global warming has now been declared a scientific certainty.<br />
-Companies for legal and tax reasons need to reduce their carbon footprint. <br />
-So lets plant some trees, but who ever said those trees cant grow food?<br />
-Over time this can only grow in size for the main cost being land acquisition, infrastructure, equipment and planting<br />
-Vast quantity of food will be available for those that need it the most.<br />
-This after all, is nothing more than a box to tick upon company registration,<br />
-a percentage of sales to allocate, and a donation to balance out at the end of the financial year.<br />
-A simple idea, massive impact, and one of the countless plans we have on the table.<br />
-	
-			<br />
-			</div>
-		</div>
-</div>
-
-?>
+     <div id="block">
+         <div id="txt">
+            <br />
+            This is earth. Over 7 billion people live here. <br />
+            A child starves to death every 4 seconds.<br />
+            21 children die every second of every day from diarrhoea. <br />
+            123 children die every second before they reach the age of 5.<br />
+            Over a billion people cant read or even write their own name.<br />
+            With 1% of the of the money spent on weapons, every child could be in school<br />
+            1.1 billion people need a proper water supply.<br />
+            2.6 billion people need basic sanitation.<br />
+            At least 80% of humanity lives on less than $10 a day.<br />
+            Almost half of the world population live on less than $2.50 a day.<br />
+            More than 1.3 billion live in extreme poverty less than $1.25 a day.<br />
+            50% of the worlds population account for less than 1% of the worlds wealth.<br />
+            The top 1% of the worlds population accounts for just under half the worlds wealth.<br />
+            The top 10% of the worlds population holds almost 90% of the worlds wealth.<br />
+            Time to make a change.<br />
+            <br />
+            No more bottom line thinking or shareholder profits
+            The worlds most desperate people are our dividend. <br />
+            We are not a standard charity begging for money, we believe in self sustaining charity.<br />
+            We believe in creating a self sustaining pool of income that’s always there and ever growing to make a change and make a difference. <br />
+            The seed that we will plant to grow the forest, here, now, today is simple. <br />
+            We will happily provide feature rich technological solutions to make your life better<br />
+            By simply using our services you change the world<br />
+            Make a difference today, join now<br />
+            </div>
+        </div>
+    </div>
 </body>
 </html>
 
